69e544e34339d0dbcc926082a6533d75f893c713
Rohith M <rohithmerala@gmail.com>
2025-10-26 10:47:45 +0800

    Initial Commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1>UNDER CONSTRUCTION⚠️</h1>
+            <h2>ROHITH PORTFOLIO</h2>
+        </>
+
+    )
+}
+
+export default App
